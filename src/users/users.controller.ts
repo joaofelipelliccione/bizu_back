baf6a5a44a525212279c4cc63f810fbe03bfd14f
João Felipe P <joaofelipelliccione@JFs-MacBook-Pro.local>
2022-08-30 15:48:51 -0300

    Melhorando código!

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,8 +8,8 @@ import {
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from '../auth/auth.service';
+import { LocalAuthGuard } from '../auth/local-auth.guard';
 // import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users') // Nome da rota
@@ -29,7 +29,7 @@ export class UsersController {
     return this.usersService.findAll();
   }
 
-  @UseGuards(AuthGuard('local')) // O endpoint abaixo só será acessado quando receber uma requisição de login válida.
+  @UseGuards(LocalAuthGuard) // O endpoint abaixo só será acessado quando receber uma requisição de login válida.
   @Post('/login')
   async login(@Request() req) {
     return this.authService.login(req.user);
