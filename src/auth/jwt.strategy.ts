8140d7c51b8ed1f8c80041d7b0bcaf4fe2ed933a
João Felipe P <joaofelipelliccione@JFs-MacBook-Pro.local>
2022-09-17 11:57:09 -0300

    Continuando desenvolvimento!

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,13 +1,24 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
+import { Request } from 'express';
 import 'dotenv/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => {
+          const token = request?.cookies['accessToken'];
+
+          if (!token) {
+            return null;
+          }
+
+          return token;
+        },
+      ]),
       ignoreExpiration: false,
       secretOrKey: process.env.JWT_SECRET,
     });
