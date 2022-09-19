8c3a817f832f465fb1e8ff372073ed339d5fce33
João Felipe P <joaofelipelliccione@JFs-MacBook-Pro.local>
2022-09-19 18:33:23 -0300

    Continuando desenvolvimento!

--- src/database/dataSource.ts
+++ src/database/dataSource.ts
@@ -4,7 +4,7 @@ import 'dotenv/config';
 const dataSource = new DataSource({
   type: 'mysql',
   host: process.env.HOST,
-  port: Number(process.env.PORT),
+  port: 3306,
   username: process.env.JAWS_DB_USERNAME,
   password: process.env.JAWS_DB_PASSWORD,
   database: process.env.JAWS_DB_NAME,
