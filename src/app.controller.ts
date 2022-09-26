45f4127740188b8cf6e6c6789d6a11c2f87f04c3
João Felipe P <joaofelipelliccione@JFs-MacBook-Pro.local>
2022-09-26 13:37:34 -0300

    Continuando desenvolvimento!

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
-import { Throttle } from '@nestjs/throttler';
 
 @Controller()
 export class AppController {
   // PING:
-  @Throttle(5, 120)
   @Get()
   async ping() {
     return {
