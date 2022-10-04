0f25fd715125a50b29cc7c6d6805a70f0509741a
João Felipe P <joaofelipelliccione@JFs-MacBook-Pro.local>
2022-10-04 00:35:29 -0300

    Continuando desenvolvimento!

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:3000/', credentials: true });
   app.use(cookieParser());
+  app.enableCors();
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
