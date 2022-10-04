b817bcf06e687e8af553eb1f6e3dcba13af8d373
João Felipe P <joaofelipelliccione@JFs-MacBook-Pro.local>
2022-10-04 10:58:02 -0300

    Continuando desenvolvimento!

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://bizudesign.vercel.app'],
+    origin: process.env.BASE_URL_FRONT,
     allowedHeaders: ['Content-Type', 'Authorization', 'x-csrf-token'],
     credentials: true,
     exposedHeaders: ['*', 'Authorization'],
