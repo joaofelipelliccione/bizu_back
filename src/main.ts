686fd01bb5c879db2eab27a1e0ba4336dcd58e49
João Felipe P <joaofelipelliccione@JFs-MacBook-Pro.local>
2022-10-04 00:45:36 -0300

    Continuando desenvolvimento!

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: 'https://bizudesign.vercel.app/',
+    origin: 'https://bizudesign.vercel.app',
     credentials: true,
   });
   await app.listen(process.env.PORT || 3001);
